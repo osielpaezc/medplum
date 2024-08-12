1bda7d8ca38dcd9699e5a9b5e4c4c0528d115da9
Osiel Paez Lima <osiel.paez@cordina.co>
2024-08-11 20:16:41 -0400

    working

--- cordina/plugins/chms.ts
+++ cordina/plugins/chms.ts
@@ -1,6 +1,7 @@
 import { MedplumClient, type MedplumClientOptions } from '@medplum/core';
 
 export default defineNuxtPlugin(() => {
+  
   const medplum = ref<MedplumClient | null>(null);
   const instance = computed(() => medplum.value);
 
@@ -8,16 +9,18 @@ export default defineNuxtPlugin(() => {
     baseUrl: useRuntimeConfig().public.medplumBaseUrl,
     clientId: useRuntimeConfig().public.medplumClientId,
     clientSecret: useRuntimeConfig().public.medplumClientSecret,
-    // onUnauthenticated: () => {
-    //   console.log('User is unauthenticated. Redirecting to login...');
-    //   //useRouter().push('/logoff');
-    // },
+    onUnauthenticated: () => {
+      const chmsStore = useChmsStore()
+      console.log('CHMS is getting user unauthenticated');
+      chmsStore.setLoggedIn(false)
+      //useRouter().push('/logoff');
+    },
   } as MedplumClientOptions;
 
   if (!medplum.value) medplum.value = new MedplumClient(options);
 
   const authorize = async (userId: string, accessToken: string) => {
-    return await instance.value.exchangeExternalAccessToken(accessToken);
+    return await instance.value?.exchangeExternalAccessToken(accessToken);
   };
 
   return {
--- cordina/stores/chms.ts
+++ cordina/stores/chms.ts
@@ -1,16 +1,55 @@
 import { storeToRefs } from 'pinia';
-import type { ProfileResource } from '@medplum/core';
+import type { MedplumClientEventMap, ProfileResource } from '@medplum/core';
 
 // SECTION Store
 export const useChmsStore = defineStore('Cordina HMS', () => {
   const { $chms } = useNuxtApp();
 
-  // store states
+  /// --------------------
+  //  store state
+  /// --------------------
   const isUserAuthenticated = ref<boolean>(false);
   const isUserAuthorized = ref<boolean>(false);
+  // const isUserPserrojectAdmin = ref($chms?.isProjectAdmin());
+  // const isUserSuperAdmin = ref($chms?.isSuperAdmin());
   const profile = ref<ProfileResource | null>(null);
   const userId = ref<string | undefined>(undefined);
 
+
+  /// --------------------
+  //  store getters
+  /// --------------------
+  // const theme = cookieRef('theme', themeConfig.app.theme)
+
+  /// --------------------
+  //  store actions
+  /// --------------------
+  const $reset = () => {
+    isUserAuthenticated.value = false;
+    isUserAuthorized.value = false;
+    profile.value = null;
+  };
+
+  const setLoggedIn = (isLogedIn: boolean) => {
+    isUserAuthenticated.value = isLogedIn;
+  };
+
+  const getAuthorized = async (accessToken: string): Promise<void> => {
+    await $chms
+      ?.exchangeExternalAccessToken(accessToken)
+      .then((resp) => {
+        console.info('CHMS have successfully authorized user %s', userId.value);
+        isUserAuthorized.value = true;
+        profile.value = resp as ProfileResource;
+      })
+      .catch((err) => {
+        console.info('CHMS could not authorized user %s, becouse %s', userId.value, (err as Error).message);
+      });
+  };
+
+  /// --------------------
+  //  store reactive logic
+  /// --------------------
   watch(
     isUserAuthenticated,
     async (val) => {
@@ -27,55 +66,15 @@ export const useChmsStore = defineStore('Cordina HMS', () => {
     { immediate: true }
   );
 
-  // const theme = cookieRef('theme', themeConfig.app.theme)
-
-  // // 👉 isVerticalNavSemiDark
-  // const isVerticalNavSemiDark = cookieRef('isVerticalNavSemiDark', themeConfig.verticalNav.isVerticalNavSemiDark)
-
-  // // 👉 isVerticalNavSemiDark
-  // const skin = cookieRef('skin', themeConfig.app.skin)
-
-  // ℹ️ We need to use `storeToRefs` to forward the state
-  // const {
-  //   isLessThanOverlayNavBreakpoint,
-  //   appContentWidth,
-  //   navbarType,
-  //   isNavbarBlurEnabled,
-  //   appContentLayoutNav,
-  //   isVerticalNavCollapsed,
-  //   footerType,
-  //   isAppRTL,
-  // } = storeToRefs(useLayoutConfigStore())
-
-  // store stters
-  const setLoggedIn = (isLogedIn: boolean) => {
-    isUserAuthenticated.value = isLogedIn;
-  };
-
-  const getAuthorized = async (accessToken: string) => {
-    await $chms
-      ?.exchangeExternalAccessToken(accessToken)
-      .then((resp) => {
-        console.info('CHMS have successfully authorized user %s', userId.value);
-        isUserAuthorized.value = true;
-        profile.value = resp as ProfileResource;
-      })
-      .catch((err) => {
-        console.info('CHMS could not authorized user %s, becouse %s', userId.value, (err as Error).message);
-      });
-  };
-
-  const $reset = () => {
-    isUserAuthenticated.value = false;
-    isUserAuthorized.value = false;
-    profile.value = null;
-  };
-
-  // return store state
+  /// ----------------------
+  //  store accesible state
+  /// ----------------------
   return {
     setLoggedIn,
     isUserAuthenticated,
     isUserAuthorized,
+    isProjectAdmin: $chms?.isProjectAdmin(),
+    isSuperAdmin: $chms?.isSuperAdmin(),
     profile,
   };
 });
@@ -84,12 +83,37 @@ export const useChmsStore = defineStore('Cordina HMS', () => {
 // SECTION Init
 export const initChmsStore = () => {
   const { status } = useAuth();
+  const { $chms } = useNuxtApp();
   const { setLoggedIn } = useChmsStore();
 
   watch(status, (_status) => setLoggedIn(_status == 'authenticated'));
 
+  // Event listener function
+  function eventListener(type: any): void {
+    console.info('medplum event `%s`', type);
+  }
+
+  // List of events to track
+  const EVENTS_TO_TRACK: (keyof MedplumClientEventMap)[] = [
+    'change',
+    'storageInitialized',
+    'storageInitFailed',
+    'profileRefreshing',
+    'profileRefreshed',
+  ];
+
+  // Setup event listeners on component mount
   onMounted(() => {
-    // do nothing here yet
+    for (const event of EVENTS_TO_TRACK) {
+      $chms?.addEventListener(event, eventListener);
+    }
+  });
+
+  // Cleanup event listeners on component unmount
+  onUnmounted(() => {
+    for (const event of EVENTS_TO_TRACK) {
+      $chms?.removeEventListener(event, eventListener);
+    }
   });
 };
 // !SECTION
